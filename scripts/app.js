c88adda1e76969f11513a1c7f432e7b7c644496c
ChristmasLover12345 <madnes0205@gmail.com>
2024-11-30 18:55:15 -0800

    made random button work on the deepwoken page

--- scripts/app.js
+++ scripts/app.js
@@ -10,4 +10,43 @@ let buildName = document.getElementById('buildName');
 let buildDesc = document.getElementById('buildDesc');
 let buildLink = document.getElementById('buildLink');
 
-// DEEPWOKEN
+
+
+
+function getBuildData()
+{
+   return fetch('../data/data.json')
+    .then( response => response.json())
+    .then( data => {
+        
+        return data.deepwokenBuilds
+    })
+}
+
+
+function getRandomBuild(deepwokenBuilds)
+{
+    let randomIndex = Math.floor(Math.random() * deepwokenBuilds.length );
+    
+    return deepwokenBuilds[randomIndex];
+}
+
+
+
+randomBtn.addEventListener('click', () => 
+    {
+        
+        getBuildData()
+        .then(deepwokenBuilds => {
+            let randomBuild = getRandomBuild(deepwokenBuilds);
+            
+           
+    
+        buildName.innerText = randomBuild.buildName
+        buildDesc.innerText = randomBuild.buildDesc
+        buildLink.href = randomBuild.buildLink
+    
+    })
+    })
+
+    // DEEPWOKEN
