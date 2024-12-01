3af4fe70b4c9c1fcc6a7435da7674051881311ad
ChristmasLover12345 <madnes0205@gmail.com>
2024-11-30 18:35:30 -0800

    app start

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,13 @@
 
 
 
+let gunBtn = document.getElementById('gunBtn');
+let galeBtn = document.getElementById('galeBtn');
+let maestroBtn = document.getElementById('maestroBtn');
+let randomBtn = document.getElementById('randomBtn');
+
+let buildName = document.getElementById('buildName');
+let buildDesc = document.getElementById('buildDesc');
+let buildLink = document.getElementById('buildLink');
+
+
